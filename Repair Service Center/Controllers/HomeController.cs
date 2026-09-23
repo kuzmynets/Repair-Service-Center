2e165c297ed09aa16f1337c97ca541504eef20a1
Max Didyk <pubgtop555@gmail.com>
2026-09-23 13:42:32 +0300

    Init commit

---
+++ Repair Service Center/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Repair_Service_Center.Models;
+
+namespace Repair_Service_Center.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Repair Service Center/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Repair_Service_Center.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
